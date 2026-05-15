7119f8265b0b7aa6a0168cea1b76e21755c28b60
Keskin Karaagac <130964010+KeskinKaraagac@users.noreply.github.com>
2026-05-15 14:06:23 +0100

    Add MySettings route to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { CompanyProvider } from "./context/CompanyContext";
 import { LanguageProvider } from "./context/LanguageContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
+import MySettings from "./pages/MySettings";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -27,6 +28,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/my-settings" element={<MySettings />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
 
